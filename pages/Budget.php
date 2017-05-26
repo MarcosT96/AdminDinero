576403de5f1fdd4570d24a7687c81f7a34d2380f
Marcos Tomassi <marcostomassi@outlook.com>
2017-05-26 13:36:17 -0300

    Create Budget.php

---
+++ pages/Budget.php
@@ -0,0 +1,84 @@
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Administrar Presupuesto</h1>
+                </div>
+            </div>
+            <div class="row">
+                  <div class="col-lg-6 ">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            Ajustes de Presupuesto
+                        </div>
+                            <div class="panel-body">
+                                <form role="form">
+                                    <fieldset>
+                                    <div class="form-group pull-left">
+                                        <label for="name">Categoria</label>
+                                        <select name="month" class="form-control">
+                                            <option>January</option>
+                                          </select>
+                                    </div>
+                                    <div class="form-group pull-right">
+                                         <label for="amount">Cantidad</label>
+                                        <input class="form-control" required placeholder="amount" width="10" name="amount" type="text" value="">
+                                   </div>
+                                   <hr class="clearbothh">
+                                  
+                                   <div class="form-group pull-left clearbothh">
+                                        <label for="month">Mes</label>
+                                        <select name="month" class="form-control">
+                                            <option>Enero</option>
+                                            <option>Ferbrero</option>
+                                            <option>Marzo</option>
+                                            <option>Abril</option>
+                                            <option>Mayo</option>
+                                            <option>Junio</option>
+                                            <option>Julio</option>
+                                            <option>Agosto</option>
+                                            <option>Septiembre</option>
+                                            <option>Octubre</option>
+                                            <option>Noviembre</option>
+                                            <option>Diciembre</option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group pull-left">
+                                         <label for="year">Año</label>
+                                        <select name="year" class="form-control">
+                                            <option></option>
+                                            <option>2015</option>
+                                            <option>2016</option>
+                                            <option>2017</option>
+                                        </select>
+                                   </div>
+                                   
+                                                              
+                                </fieldset>
+                                </form>
+                            </div>
+                            <div class="panel-footer">
+                            <button type="submit" class="btn btn-warning btn-block"><span class="glyphicon glyphicon-log-in"></span>  Ahorre presupuesto</button>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-lg-5">
+                <div class="panel panel-green">
+                        <div class="panel-heading">
+                            Presupuesto basado en el gráfico
+                        </div>
+                        <div class="panel-body">
+                            <p>Agregue su presupuesto con su categoría y por mes y año </p>
+                        </div>
+                        <div class="panel-footer">
+                           
+                        </div>
+                    </div>
+                </div>   
+            </div>
+
+        </div>
+ 
+
+  
+
